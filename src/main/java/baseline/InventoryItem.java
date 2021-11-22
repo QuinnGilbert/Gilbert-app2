993fe3187f85ea976953f779ef940394ad0f0c73
QuinnG <quinngilbert12and3@gmail.com>
2021-11-21 22:00:01 -0500

    Fixed many functions

--- src/main/java/baseline/InventoryItem.java
+++ src/main/java/baseline/InventoryItem.java
@@ -6,8 +6,12 @@ package baseline;
 
 import javafx.beans.property.SimpleStringProperty;
 
+import java.util.HashSet;
+
 public class InventoryItem {
     //InventoryItem will hold data for a single row in the table
+
+    private static HashSet<String> serialSet = new HashSet<>();
 
     private final SimpleStringProperty name;
     private final SimpleStringProperty serial;
@@ -42,4 +46,49 @@ public class InventoryItem {
         this.value.set(value);
     }
 
+    public boolean testSerial(){
+        //test if meets format
+        String s = serial.get();
+        if(s.length()!=13) return false;
+        if(!( (s.charAt(0)>='A'&&s.charAt(0)<='Z') || (s.charAt(0)>='a'&&s.charAt(0)<='z') )) return false;
+        if(!(s.charAt(1)=='-'&&s.charAt(9)=='-'&&s.charAt(5)=='-')) return false;
+        //check if the appropriate values are either numbers or digits
+        for(int i=0;i<3;i++){
+            int start = i*4+2;
+            for(int j=start;j<start+3;j++){
+                if(!( (s.charAt(j)<='9'&&s.charAt(j)>='0')|| (s.charAt(j)>='A'&&s.charAt(j)<='Z') || (s.charAt(j)>='a'&&s.charAt(j)<='z') )) return false;
+            }
+        }
+
+        //test if the serial has already been used
+        if(serialSet.contains(s)) return false;
+
+        return true;
+    }
+
+    public boolean testValue(){
+        String s = value.get();
+        if(s.equals("")) return false;
+        for(int i:s.toCharArray()){
+            if(!( i<='9'&&i>='0' )) return false;
+        }
+        return Integer.parseInt(s)>0;
+    }
+
+    public boolean testName(){
+        String s = name.get();
+        return s.length()<=256 && s.length()>=2;
+    }
+
+    static void addSerial(String serial){
+        serialSet.add(serial);
+    }
+
+    static void removeSerial(String serial){
+        serialSet.remove(serial);
+    }
+
+    static void clearSerialSet(){
+        serialSet.clear();
+    }
 }
--- src/main/java/baseline/MainMenuController.java
+++ src/main/java/baseline/MainMenuController.java
@@ -8,17 +8,23 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MainMenuController {
 
@@ -59,12 +65,30 @@ public class MainMenuController {
         //add data contained in textfields to tableview
         InventoryItem newItem = new InventoryItem(addName.getText(),addSerial.getText(),addValue.getText());
         //test if newItem is valid (and output appropriate messages if isn't
+        if(!newItem.testName()){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Name", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+        if(!newItem.testSerial()){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Serial", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+        if(!newItem.testValue()){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Value", ButtonType.OK);
+            alert.showAndWait();
+            return;
+        }
+
+        InventoryItem.addSerial(newItem.getSerial());
         data.add(newItem);
         table.setItems(data);
     }
 
     @FXML
     void remove(ActionEvent event) {
+        InventoryItem.removeSerial(table.getSelectionModel().getSelectedItem().getSerial());
         table.getItems().removeAll(table.getSelectionModel().getSelectedItem());
     }
 
@@ -79,21 +103,224 @@ public class MainMenuController {
     void clearAll(ActionEvent event) {
         //remove all values from list
         data.clear();
-    }
-
-    @FXML
-    void loadTable(ActionEvent event) {
-        //open file
-        //read in data from file
-        //update list with data
-        //update table
+        InventoryItem.clearSerialSet();
     }
 
     @FXML
     void saveTable(ActionEvent event) {
         //Open file
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter HTML = new FileChooser.ExtensionFilter("HTML","*.html");
+        FileChooser.ExtensionFilter JSON = new FileChooser.ExtensionFilter("JSON","*.json");
+        FileChooser.ExtensionFilter TSV = new FileChooser.ExtensionFilter("TSV","*.txt");
+        fileChooser.getExtensionFilters().addAll(HTML,JSON,TSV);
+        Stage stage = new Stage();
+        File file = fileChooser.showSaveDialog(stage);
+
+        System.out.println(getExtension(file));
+        if(getExtension(file).equals("txt")){
+            saveTSV(file);
+        }
+        if(getExtension(file).equals("html")){
+            saveHTML(file);
+        }
+        if(getExtension(file).equals("json")){
+            saveJSON(file);
+        }
         //get string with properly formatted data
         //save data to file
+    }
+
+    String getExtension(File file){
+        String filename = file.getName();
+        return filename.substring(filename.lastIndexOf(".") + 1);
+    }
+
+    void saveTSV(File file){
+        StringBuilder listData=new StringBuilder("SerialNumber\tName\tValue\n");
+        for(InventoryItem i : data){
+            listData.append(i.getSerial()+"\t"+i.getName()+"\t"+i.getValue()+"\n");
+        }
+
+        if(file!=null){
+            try{
+                PrintWriter out = new PrintWriter(file);
+                out.println(listData);
+                out.close();
+            } catch(IOException ex){
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    void saveHTML(File file){
+        StringBuilder listData=new StringBuilder("<!DOCTYPE html><html><body><table>\n<tr><th>Serial</th><th>Name</th><th>Value</th></tr>\n");
+        for(InventoryItem i : data){
+            listData.append("<tr><td>"+i.getSerial()+"</td><td>"+i.getName()+"</td><td>"+i.getValue()+"</td></tr>\n");
+        }
+        listData.append("</table></body></html>");
+        if(file!=null){
+            try{
+                PrintWriter out = new PrintWriter(file);
+                out.println(listData);
+                out.close();
+            } catch(IOException ex){
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    void saveJSON(File file){
+
+        ArrayList<String> name = new ArrayList<>();
+        ArrayList<String> serial = new ArrayList<>();
+        ArrayList<String> value = new ArrayList<>();
+        for(InventoryItem i : data){
+            name.add(i.getName());
+            serial.add(i.getSerial());
+            value.add(i.getValue());
+        }
+        StringBuilder listData=new StringBuilder("{");
+        listData.append("\n\"name\":[");
+        for(int i=0;i<name.size();i++){
+            listData.append("\""+name.get(i)+"\"");
+            if(i!=name.size()-1) listData.append(",");
+        }
+        listData.append("],\n\"serial\":[");
+        for(int i=0;i<serial.size();i++){
+            listData.append("\""+serial.get(i)+"\"");
+            if(i!=serial.size()-1) listData.append(",");
+        }
+        listData.append("],\n\"value\":[");
+        for(int i=0;i<value.size();i++){
+            listData.append("\""+value.get(i)+"\"");
+            if(i!=value.size()-1) listData.append(",");
+        }
+        listData.append("]\n}");
+        if(file!=null){
+            try{
+                PrintWriter out = new PrintWriter(file);
+                out.println(listData);
+                out.close();
+            } catch(IOException ex){
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    @FXML
+    void loadTable(ActionEvent event) {
+        //open file
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter ext = new FileChooser.ExtensionFilter("List Files:","*.html","*.json","*.txt");
+//        FileChooser.ExtensionFilter JSON = new FileChooser.ExtensionFilter("JSON","*.json");
+//        FileChooser.ExtensionFilter TSV = new FileChooser.ExtensionFilter("TSV","*.txt");
+        fileChooser.getExtensionFilters().addAll(ext);
+        Stage stage = new Stage();
+        File file = fileChooser.showOpenDialog(stage);
+
+        if(getExtension(file).equals("txt")){
+            loadTSV(file);
+        }
+        if(getExtension(file).equals("html")){
+            loadHTML(file);
+        }
+        if(getExtension(file).equals("json")){
+            loadJSON(file);
+        }
+        //read in data from file
+        //update list with data
+        //update table
+        //Update serial set with all values
+    }
+
+    void loadTSV(File file){
+        if(file!=null) {
+            data.clear();
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(file));
+                in.readLine();
+                while (in.ready()) {
+                    StringTokenizer st = new StringTokenizer(in.readLine());
+                    String serial = st.nextToken();
+                    String name = st.nextToken();
+                    String value = st.nextToken();
+                    data.add(new InventoryItem(name,serial,value));
+                }
+                in.close();
+            } catch (IOException ex) {
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            table.setItems(data);
+        }
+    }
+
+    void loadHTML(File file){
+        if(file!=null) {
+            data.clear();
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(file));
+                in.readLine();
+                in.readLine();
+                while (in.ready()) {
+                    String line = in.readLine();
+                    String[] values = new String[3];
+                    values[0] = "";
+                    values[1] = "";
+                    values[2] = "";
+                    int index=0;
+                    boolean write = false;
+                    for(char i:line.toCharArray()){
+                        if(i=='<'){
+                            write=false;
+                            index++;
+                        }
+                        if(write){
+                            values[(index-1)/2]+=i;
+                        }
+                        if(i=='>'){
+                            write=true;
+                        }
+                    }
+                    data.add(new InventoryItem(values[1],values[0],values[2]));
+                }
+                in.close();
+            } catch (IOException ex) {
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            table.setItems(data);
+        }
+    }
+
+    void loadJSON(File file){
+        if(file!=null) {
+            data.clear();
+            try {
+                BufferedReader in = new BufferedReader(new FileReader(file));
+                in.readLine();
+                String namesString = in.readLine();
+                String serialString = in.readLine();
+                String valueString = in.readLine();
+                namesString = namesString.substring(8,namesString.length()-2);
+                serialString = serialString.substring(10,serialString.length()-2);
+                valueString = valueString.substring(9,valueString.length()-1);
+                String[] nameList = namesString.split(",");
+                String[] serialList = serialString.split(",");
+                String[] valueList = valueString.split(",");
+
+//                ArrayList<String> name = new ArrayList<>();
+//                ArrayList<String> serial = new ArrayList<>();
+//                ArrayList<String> value = new ArrayList<>();
+
+                for(int i=0;i<nameList.length;i++){
+                    data.add(new InventoryItem(nameList[i].substring(1,nameList[i].length()-1),serialList[i].substring(1,serialList[i].length()-1),valueList[i].substring(1,valueList[i].length()-1)));
+                }
+                in.close();
+            } catch (IOException ex) {
+                Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            table.setItems(data);
+        }
     }
 
     @FXML
@@ -135,7 +362,64 @@ public class MainMenuController {
         serial.setCellValueFactory(new PropertyValueFactory<InventoryItem,String>("serial"));
         value.setCellValueFactory(new PropertyValueFactory<InventoryItem,String>("value"));
 
-        //add functions to change values
+        //functions to edit values
+        table.setEditable(true);
+        name.setCellFactory(TextFieldTableCell.forTableColumn());
+        name.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<InventoryItem,String>>(){
+            @Override
+            //validate name on edit
+            public void handle(TableColumn.CellEditEvent<InventoryItem,String> event){
+                String oldValue = event.getOldValue();
+                InventoryItem item = event.getRowValue();
+                item.setName(event.getNewValue());
+                if(!item.testName()){
+                    item.setName(oldValue);
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Name", ButtonType.OK);
+                    alert.showAndWait();
+                    table.getColumns().get(0).setVisible(false);
+                    table.getColumns().get(0).setVisible(true);
+                    return;
+                }
+            }
+        });
+        serial.setCellFactory(TextFieldTableCell.forTableColumn());
+        serial.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<InventoryItem,String>>(){
+            @Override
+            //validate serial on edit
+            public void handle(TableColumn.CellEditEvent<InventoryItem,String> event){
+                String oldValue = event.getOldValue();
+                InventoryItem item = event.getRowValue();
+                item.setSerial(event.getNewValue());
+                if(!item.testSerial()){
+                    item.setSerial(oldValue);
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Serial", ButtonType.OK);
+                    alert.showAndWait();
+                    table.getColumns().get(1).setVisible(false);
+                    table.getColumns().get(1).setVisible(true);
+                    return;
+                }
+                InventoryItem.removeSerial(oldValue);
+                InventoryItem.addSerial(item.getSerial());
+            }
+        });
+        value.setCellFactory(TextFieldTableCell.forTableColumn());
+        value.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<InventoryItem,String>>(){
+            @Override
+            //validate value on edit
+            public void handle(TableColumn.CellEditEvent<InventoryItem,String> event){
+                String oldValue = event.getOldValue();
+                InventoryItem item = event.getRowValue();
+                item.setValue(event.getNewValue());
+                if(!item.testValue()){
+                    item.setValue(oldValue);
+                    Alert alert = new Alert(Alert.AlertType.ERROR, "Invalid Value", ButtonType.OK);
+                    alert.showAndWait();
+                    table.getColumns().get(2).setVisible(false);
+                    table.getColumns().get(2).setVisible(true);
+                    return;
+                }
+            }
+        });
 
         data = FXCollections.observableArrayList(
                 new InventoryItem("name1","serial1","1"),
